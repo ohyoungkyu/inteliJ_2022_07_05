70cc1f5ef581218017b8f7b173ce6d4346c04db1
ohyoungkyu <come8908@gmail.com>
2022-07-05 19:17:36 +0900

    Article api 구현

---
+++ src/main/java/com/mysite/sbb/Controller/ArticleController.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.Controller;
+
+import com.mysite.sbb.Dao.ArticleRepository;
+import com.mysite.sbb.Domain.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("usr/article")
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Article> showList() {
+        return articleRepository.findAll();
+    }
+
+
+}
