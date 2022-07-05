7aea4d382860dd1ca54a69a97ab6be3f3c9b65e2
ohyoungkyu <come8908@gmail.com>
2022-07-05 19:24:35 +0900

    User api 구현

---
+++ src/main/java/com/mysite/sbb/Dao/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb.Dao;
+
+import com.mysite.sbb.Domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
