090960ed5a24967139e169005bc48f73d6926d21
ohyoungkyu <come8908@gmail.com>
2022-07-22 19:31:57 +0900

    시큐리티,회원가입,로그인과 로그아웃 기능 구현

--- src/main/java/com/mysite/sbb/Controller/UserController.java
+++ src/main/java/com/mysite/sbb/Controller/UserController.java
@@ -1,30 +1,58 @@
 package com.mysite.sbb.Controller;
 
-import com.mysite.sbb.Dao.UserRepository;
-import com.mysite.sbb.Domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mysite.sbb.Form.UserCreateForm;
+import com.mysite.sbb.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import javax.validation.Valid;
 
+@RequiredArgsConstructor
 @Controller
-@RequestMapping("/api/users")
+@RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
 
-    @RequestMapping("")
-    @ResponseBody
-    public List<User> users(){
-        return userRepository.findAll();
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
     }
 
-    @RequestMapping("1")
-    @ResponseBody
-    public User user(){
-        User user = new User();
-        return user;
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
     }
 }
---
+++ src/main/java/com/mysite/sbb/Service/UserService.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.Service;
+
+import com.mysite.sbb.Dao.UserRepository;
+import com.mysite.sbb.Domain.SiteUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password) {
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
